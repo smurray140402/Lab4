4c1478da748e55d0046d18263aa8efc5c701dab5
smurray140402 <g00426904@atu.ie>
2025-02-24 10:15:29 +0000

    Lab 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count:number = 0;
+  show:boolean = false;
+
+  onButtonClick() {
+    this.count++;
+  }
+
+  starClicked() {
+    if (this.show) {
+      this.show = false;
+    }
+    else {
+      this.show = true;
+    }
+  }
 }
